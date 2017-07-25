4d360e8d677a312682b21103f19fad64d6bf7e62
JakeGreer <Jakegreer11@gmail.com>
2017-07-25 11:15:07 -0700

    final commit

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -6,7 +6,7 @@ function displayGifs() {
 
 	$("#images").empty();
 	 var gif = $(this).attr("data-name");
-	 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC";
+	 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC";
 
 	 $.ajax({
 	  url: queryURL,
